d0774659878143f5233a3f0d00534f771cb8323a
gltknky <gltknky@gmail.com>
2020-04-29 21:06:01 +0300

    added rtc chat code

--- src/app/pages/video-call/video-call.page.ts
+++ src/app/pages/video-call/video-call.page.ts
@@ -1,27 +1,147 @@
-import { Component, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import * as firebase from "firebase";
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import * as firebase from "firebase/app";
+import { Observable } from 'rxjs';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+declare let RTCPeerConnection: any;
 
 @Component({
-  selector: "app-video-call",
-  templateUrl: "./video-call.page.html",
-  styleUrls: ["./video-call.page.scss"],
+  selector: 'app-meeting',
+  templateUrl: './video-call.page.html',
+  styleUrls: ['./video-call.page.scss']
 })
 export class VideoCallPage implements OnInit {
-  getOpenTokSession = firebase.functions().httpsCallable("getOpenTokSession");
-  constructor(public httpClient: HttpClient) {}
+  callActive: boolean = false;
+  pc: any;
+  localStream: any;
+  channel: AngularFireList<{}>;
+  database: firebase.database.Reference;
+  senderId: string;
+
+  @ViewChild("me", { static: true }) me: any;
+  @ViewChild("remote", { static: true }) remote: any;
+
+  constructor(
+    private afDb: AngularFireDatabase,
+  ) { }
+
   ngOnInit() {
-    this.getOpenTokSession().then(
-      (result: firebase.functions.HttpsCallableResult) => {
-        console.log(result.data);
-      }
-    );
-    // this.httpClient
-    //   .get(
-    //     "https://us-central1-video-call-app-5bb0c.cloudfunctions.net/helloWorld"
-    //   )
-    //   .subscribe((res) => {
-    //     console.log(res);
-    //   });
+    this.setupWebRtc();
   }
+
+  public ngOnDestroy() {
+    this.pc.close();
+    let tracks = this.localStream.getTracks();
+    for (let i = 0; i < tracks.length; i++) {
+      tracks[i].stop();
+    }
+    this.callActive = false;
+  }
+
+  setupWebRtc() {
+    this.senderId = this.guid();
+    var channelName = "/webrtc";
+    this.channel = this.afDb.list(channelName);
+    this.database = this.afDb.database.ref(channelName);
+    this.database.on("child_added", this.readMessage.bind(this));
+
+    try {
+      this.pc = new RTCPeerConnection({
+        iceServers: [
+          { urls: "stun:stun.services.mozilla.com" },
+          { urls: "stun:stun.l.google.com:19302" }
+        ]
+      }, { optional: [] });
+    } catch (error) {
+      console.log(error);
+      this.pc = new RTCPeerConnection({
+        iceServers: [
+          { urls: "stun:stun.services.mozilla.com" },
+          { urls: "stun:stun.l.google.com:19302" }
+        ]
+      }, { optional: [] });
+    }
+
+
+    this.pc.onicecandidate = event => {
+      event.candidate ? this.sendMessage(this.senderId, JSON.stringify({ ice: event.candidate })) : console.log("Sent All Ice");
+    }
+
+    this.pc.onremovestream = event => {
+      console.log('Stream Ended');
+    }
+
+    this.pc.ontrack = event =>
+      (this.remote.nativeElement.srcObject = event.streams[0]); // use ontrack
+    this.showMe();
+  }
+
+  sendMessage(senderId, data) {
+    var msg = this.channel.push({ sender: senderId, message: data });
+    msg.remove();
+  }
+
+  readMessage(data) {
+    if (!data) return;
+    try {
+      var msg = JSON.parse(data.val().message);
+      let personalData = data.val().personalData;
+      var sender = data.val().sender;
+      if (sender != this.senderId) {
+        if (msg.ice != undefined && this.pc != null) {
+          this.pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+        } else if (msg.sdp.type == "offer") {
+          this.callActive = true;
+          this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+            .then(() => this.pc.createAnswer())
+            .then(answer => this.pc.setLocalDescription(answer))
+            .then(() => this.sendMessage(this.senderId, JSON.stringify({ sdp: this.pc.localDescription })));
+        } else if (msg.sdp.type == "answer") {
+          this.callActive = true;
+          this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  showMe() {
+    navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+      .then(stream => (this.me.nativeElement.srcObject = stream))
+      .then(stream => {
+        this.pc.addStream(stream);
+        this.localStream = stream;
+      });
+  }
+
+  showRemote() {
+    try {
+      this.pc.createOffer()
+        .then(offer => this.pc.setLocalDescription(offer))
+        .then(() => {
+          this.sendMessage(this.senderId, JSON.stringify({ sdp: this.pc.localDescription }));
+          this.callActive = true;
+        });
+    } catch (error) {
+      this.setupWebRtc();
+      console.log(error);
+    }
+  }
+
+  hangup() {
+    this.pc.close();
+    let tracks = this.localStream.getTracks();
+    for (let i = 0; i < tracks.length; i++) {
+      tracks[i].stop();
+    }
+    this.callActive = false;
+  }
+
+  guid() {
+    return (this.s4() + this.s4() + "-" + this.s4() + "-" + this.s4() + "-" + this.s4() + "-" + this.s4() + this.s4() + this.s4());
+  }
+  s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+
 }
